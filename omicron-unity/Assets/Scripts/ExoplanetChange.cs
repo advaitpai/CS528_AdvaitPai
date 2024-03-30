80e4c3731e7cf8fbb0d01ae0a0e21b1c727f7b06
Advait Pai <67473396+advaitpai@users.noreply.github.com>
2024-03-30 18:42:15 -0500

    Update ExoplanetChange.cs

--- omicron-unity/Assets/Scripts/ExoplanetChange.cs
+++ omicron-unity/Assets/Scripts/ExoplanetChange.cs
@@ -45,4 +45,37 @@ public class ExoplanetChange : StarDataLoader
     {
         
     }
+    Color exoColour(int val)
+    {
+        if (val == 1)
+        {
+            return new Color(255f/255f,127f/255f,127f/255f); // Light Red 
+        }
+        else if (val == 2)
+        {
+            return new Color(173f/255f,216f/255f,230f/255f); // Light Blue 
+        }
+        else if (val == 3)
+        {
+            return new Color(144f/255f,238f/255f,144f/255f); // Light Green
+        }
+        else if (val == 4)
+        {
+            return new Color(177f/255f,156f/255f,217f/255f); // Light Purple
+        }
+        else if (val == 5)
+        {
+            return new Color(255f/255f,255f/255f,191f/255f); // Light Yellow 
+        }
+
+        else if (val == 6)
+        {
+            return new Color(155f/255f,103f/255f,60f/255f); // Brown 
+        }
+        else
+        {
+            return Color.grey;
+        }
+
+    }
 }
